486335b34dde14cbf0090c8b85986eb116a027c0
gianMarquez <michimar.alexis@gmail.com>
2019-12-16 16:20:08 -0600

    home seletion styled

--- src/pages/home.js
+++ src/pages/home.js
@@ -31,18 +31,24 @@ export default class Home extends Component {
                   <h3>Confident</h3>
                   <div>
                     <div className="topic-introduction">
-                      <p>
-                        Practical life activities develop organization and focus
-                        skills. When the child is able to take care of himself
-                        and the environment, he feels genuine confidence.
-                      </p>
-                      <p>
-                        “The greatest development is achieved during the first
-                        years of life, and therefore it is then that the
-                        greatest care should be taken. If this is done, then the
-                        child does not become a burden; he will reveal himself
-                        as the greatest marvel of nature.” – Maria Montessori
-                      </p>
+                      <div className="p1">
+                        <h5>
+                          Practical life activities develop organization and
+                          focus skills. When the child is able to take care of
+                          himself and the environment, he feels genuine
+                          confidence.
+                        </h5>
+                      </div>
+                      <div className="p2">
+                        <h6>
+                          “The greatest development is achieved during the first
+                          years of life, and therefore it is then that the
+                          greatest care should be taken. If this is done, then
+                          the child does not become a burden; he will reveal
+                          himself as the greatest marvel of nature.” – Maria
+                          Montessori
+                        </h6>
+                      </div>
                     </div>
                     <div className="image">
                       <img src={confidentImg} />
@@ -53,26 +59,30 @@ export default class Home extends Component {
                 <div onClick={this.handleLinkClick} className="topic-big-block">
                   <h3>Enthusiastic life-long learner</h3>
                   <div>
+                    <div className="topic-introduction">
+                      <div className="p1">
+                        <h4>
+                          Cultural experiences in geography, history, zoology,
+                          botany, dance, music and art. We put the world at the
+                          child’s fingertips.
+                        </h4>
+                      </div>
+                      <div className="p2">
+                        <h4>
+                          "It is through appropriate work and activities that
+                          the character of the child is transformed. Work
+                          influences his development in the same way that food
+                          revives the vigor of a starving man. We observe that a
+                          child occupied with matters that awaken his interest
+                          seems to blossom, to expand, evincing undreamed of
+                          character traits; his abilities give him great
+                          satisfaction, and he smiles with a sweet and joyous
+                          smile." (San Remo Lectures, p. 28)
+                        </h4>
+                      </div>
+                    </div>
                     <div className="big-image">
                       <img src={world} />
-                    </div>
-                    <div className="topic-introduction">
-                      <p>
-                        Cultural experiences in geography, history, zoology,
-                        botany, dance, music and art. We put the world at the
-                        child’s fingertips.
-                      </p>
-                      <p>
-                        "It is through appropriate work and activities that the
-                        character of the child is transformed. Work influences
-                        his development in the same way that food revives the
-                        vigor of a starving man. We observe that a child
-                        occupied with matters that awaken his interest seems to
-                        blossom, to expand, evincing undreamed of character
-                        traits; his abilities give him great satisfaction, and
-                        he smiles with a sweet and joyous smile." (San Remo
-                        Lectures, p. 28)
-                      </p>
                     </div>
                   </div>
                 </div>
@@ -82,24 +92,28 @@ export default class Home extends Component {
                 <div onClick={this.handleLinkClick} className="topic-block">
                   <h3>Emotionally Intelligent</h3>
                   <div>
+                    <div className="topic-introduction">
+                      <div className="p1">
+                        <h5>
+                          Peace education helps children learn to contribute,
+                          set boundaries or respect, handle conflict wisely,
+                          understand others, collaborate, and seek after peace.
+                        </h5>
+                      </div>
+                      <div className="p2">
+                        <h6>
+                          "A child who has become master of his acts through
+                          long and repeated exercises, and who has been
+                          encouraged by the pleasant and interesting activities
+                          in which he has been engaged, is a child filled with
+                          health and joy and remarkable for his calmness and
+                          discipline." (Dr. Maria Montessori, 'The Discovery of
+                          the Child', Clio Press Ltd, 92)
+                        </h6>
+                      </div>
+                    </div>
                     <div className="image">
                       <img src={emotionalInteligent} />
-                    </div>
-                    <div className="topic-introduction">
-                      <p>
-                        Peace education helps children learn to contribute, set
-                        boundaries or respect, handle conflict wisely,
-                        understand others, collaborate, and seek after peace.
-                      </p>
-                      <p>
-                        "A child who has become master of his acts through long
-                        and repeated exercises, and who has been encouraged by
-                        the pleasant and interesting activities in which he has
-                        been engaged, is a child filled with health and joy and
-                        remarkable for his calmness and discipline." (Dr. Maria
-                        Montessori, 'The Discovery of the Child', Clio Press
-                        Ltd, 92)
-                      </p>
                     </div>
                   </div>
                 </div>
@@ -107,23 +121,27 @@ export default class Home extends Component {
                 <div onClick={this.handleLinkClick} className="topic-block">
                   <h3>A Good Comunicator</h3>
                   <div>
+                    <div className="topic-introduction">
+                      <div className="p1">
+                        <h5>
+                          Language used to connect and belong through listening,
+                          speaking, reading and writing in Spanish and English.
+                          We are a real Spanish immersion program.
+                        </h5>
+                      </div>
+                      <div className="p2">
+                        <h5>
+                          “We await the successive births in the soul of the
+                          child. We give all possible material, that nothing may
+                          lack to the growing soul, and then we watch for the
+                          perfect faculty to come, safeguarding the child from
+                          interruption so that it may carry its efforts
+                          through.” – Maria Montessori
+                        </h5>
+                      </div>
+                    </div>
                     <div className="image">
                       <img src={comunication} />
-                    </div>
-                    <div className="topic-introduction">
-                      <p>
-                        Language used to connect and belong through listening,
-                        speaking, reading and writing in Spanish and English. We
-                        are a real Spanish immersion program.
-                      </p>
-                      <p>
-                        “We await the successive births in the soul of the
-                        child. We give all possible material, that nothing may
-                        lack to the growing soul, and then we watch for the
-                        perfect faculty to come, safeguarding the child from
-                        interruption so that it may carry its efforts through.”
-                        – Maria Montessori
-                      </p>
                     </div>
                   </div>
                 </div>
@@ -131,20 +149,24 @@ export default class Home extends Component {
                 <div onClick={this.handleLinkClick} className="topic-block">
                   <h3>A Problem Solver</h3>
                   <div>
+                    <div className="topic-introduction">
+                      <div className="p1">
+                        <h4>
+                          Math exercises that are challenging, understandable
+                          and enjoyable.
+                        </h4>
+                      </div>
+                      <div className="p2">
+                        <h4>
+                          “If education is always to be conceived along the same
+                          antiquated lines of a mere transmission of knowledge,
+                          there is little to be hoped from it in the bettering
+                          of man's future.”
+                        </h4>
+                      </div>
+                    </div>
                     <div className="image">
                       <img src={solverProblem} />
-                    </div>
-                    <div className="topic-introduction">
-                      <p>
-                        Math exercises that are challenging, understandable and
-                        enjoyable.
-                      </p>
-                      <p>
-                        “If education is always to be conceived along the same
-                        antiquated lines of a mere transmission of knowledge,
-                        there is little to be hoped from it in the bettering of
-                        man's future.”
-                      </p>
                     </div>
                   </div>
                 </div>
@@ -152,26 +174,30 @@ export default class Home extends Component {
                 <div onClick={this.handleLinkClick} className="topic-block">
                   <h3>Creative</h3>
                   <div>
+                    <div className="topic-introduction">
+                      <div className="p1">
+                        <h5>
+                          Cultural experiences in geography, history, zoology,
+                          botany, dance, music and art. We put the world at the
+                          child’s fingertips.
+                        </h5>
+                      </div>
+                      <div className="p2">
+                        <h6>
+                          "It is through appropriate work and activities that
+                          the character of the child is transformed. Work
+                          influences his development in the same way that food
+                          revives the vigor of a starving man. We observe that a
+                          child occupied with matters that awaken his interest
+                          seems to blossom, to expand, evincing undreamed of
+                          character traits; his abilities give him great
+                          satisfaction, and he smiles with a sweet and joyous
+                          smile." (San Remo Lectures, p. 28)
+                        </h6>
+                      </div>
+                    </div>
                     <div className="image">
                       <img src={creative} />
-                    </div>
-                    <div className="topic-introduction">
-                      <p>
-                        Cultural experiences in geography, history, zoology,
-                        botany, dance, music and art. We put the world at the
-                        child’s fingertips.
-                      </p>
-                      <p>
-                        "It is through appropriate work and activities that the
-                        character of the child is transformed. Work influences
-                        his development in the same way that food revives the
-                        vigor of a starving man. We observe that a child
-                        occupied with matters that awaken his interest seems to
-                        blossom, to expand, evincing undreamed of character
-                        traits; his abilities give him great satisfaction, and
-                        he smiles with a sweet and joyous smile." (San Remo
-                        Lectures, p. 28)
-                      </p>
                     </div>
                   </div>
                 </div>
