5d2658c563d4b5199f50953c1e69be23a2c29eb0
gianMarquez <michimar.alexis@gmail.com>
2019-12-17 02:30:41 -0600

    updated information

--- src/pages/homepage-links/technologyAndSensoliar.js
+++ src/pages/homepage-links/technologyAndSensoliar.js
@@ -4,51 +4,122 @@ import confidentImage from "../../img/confidentImage.jpg";
 const Technology = () => {
   return (
     <div className="technology-container">
-      <div className="">
-        <h1>Technology and Sensorial</h1>
+      <div className="header">
+        <h1>
+          Sensorial works the innovative, technological, entrepreneur, creative
+          brain
+        </h1>
       </div>
       <div className="container">
         <p>
-          Through practical life activities, children of all ages develop both
-          the skills and the knowledge to participate in their social
-          surroundings effectively. These activities also promote a sense of
-          understanding for human traditions and the overall human experience of
-          providing solutions to the challenges of shelter, food, and rest.
+          These days, innovation, entrepreneurship, and technology are a hot
+          subject-- and it is no secret that some of the most prominent creative
+          minds, such as the founders of Google, Sergey Brin and Larry Page, or
+          Amazon's founder Jeff Bezos credit their Montessori foundation for
+          their ability to generate innovative solutions and think creatively.
+          In fact, many creative and bright minds in the fields of technology,
+          art, poetry, science, credit their Montessori style sensorial and
+          stereognostic experiences during their early childhood as fundamental
+          to their creative development. Such people include Thomas Edison,
+          Alexander Graham Bell, Hellen Keller, Henry Ford, Mahatma Gandhi, to
+          name a few. What could be the link between creativity, problem
+          solving, and Montessori? The answer is found in the brilliant
+          Montessori sensorial works. These works allow for quick failure, small
+          but significant progress and observation with all the senses.
         </p>
         <p>
-          Children refine their social skills in groups and intuitively work at
-          being independent. The "me do it" attitude is good. The Montessori
-          class on practical life and history studies and channels this human
-          desire to take care of one’s basic needs.
+          The technological brain is built through the senses (not screens!).
+          Through thousands of years, humans have engineered tools and buildings
+          to satisfy their basic needs. Here children will use sound, smell,
+          touch, weight, temperature, size, texture and color to solve problems
+          with a variaty of resources.
         </p>
         <p>
-          Many of the practical life activities in the cultural studies include
-          fun cultural works. For example, during our studies of Asia, children
-          may use chopsticks to transfer food. This activity would help the
-          children develop their fine motor skills, while at the same time
-          giving them an experience that connects them to Asia, and the people's
-          of the past who developed such tool. They may learn to cook a
-          traditional dish or wear a special item for a celebration.
+          Children will be entrepreneurs as they plant, grow, harvest and
+          prepare food from the garden. Children will care for hens, a bunny,
+          birds, and through this process they will stimulate their senses and
+          the problem-solving areas of the brain. They will solve
+          two-dimensional and three-dimensional problems with three-dimentional
+          and two -dimentional geometric figures. Children will build towers,
+          use simple machines machines, and solve puzzles. They will prepare
+          food, make textiles, paper, pottery, shelter, etc. They will work with
+          materials such as natural fibers, clay, marble, stones, to establish
+          the foundations of an engineering brain.
         </p>
         <p>
-          During the year we will develop skills for practical life activities
-          as we journey through time around the world. Our students will learn
-          about ancient peoples and how different human societies have satisfied
-          their basic human needs with the materials that were available to them
-          according to the resources available in their environments, their
-          climate and culture. We will go in a journey through time around the
-          world. We will visit the ancient Egyptian civilizations and the
-          pharaohs, the Greeks, Ancient China, Middle Ages, Renaissance, Ancient
-          American Civilizations and the Colonization, the founding Fathers. We
-          will experience their cultures, music, food, utensils, stories and
-          traditions.
+          The sensorial area of the Montessori curriculum was the first area
+          developed by Dr. Montessori. Through these activities, children with
+          severe developmental challenges were able to gain a sense of
+          accomplishment, improve their skills, and tackle difficulties in an
+          outstanding manner. Even though Montessori's late studies were in
+          anthropology and medicine, her first interests and studies involved
+          engeneering and math.
+        </p>
+        <p>
+          The exercises are divided in the categories of visual, tactile, baric,
+          thermic, auditory, olfactory, gustatory and sterognostic (the ability
+          to determine the shape of an object by touch or lifting it). As
+          children use the sensorial works in our class, they will gradually
+          focus and sharpen each of their senses. These works are fun and
+          enjoyable challenges that capture not only the minds, but the hands,
+          eyes, noses, and senses of the child as a whole.
+        </p>
+        <p>
+          The Montessori Sensorial works come from the word sense or senses.
+          These works are made to help the child hone his sensibilities. Such
+          exercises create a wonderful net of neurological connections that can
+          pave the way for future problem solving, and can remain for life.
+          People who have developed such gray matter networks as young children
+          can use that capacity later in life to tackle new situations,
+          conceptualize possibilities, and find creative solutions. Montessori
+          sensorial exercises are divided in the categories of visual, tactile,
+          baric, thermic, auditory, olfactory, gustatory and sterognostic (the
+          ability to determine the shape of an object by touch or lifting it).
+          As children use the sensorial works in our class, they will gradually
+          focus and sharpen each of their senses. These works are fun and
+          enjoyable challenges that capture not only the minds, but the hands,
+          eyes, noses, and senses of the child as a whole.
         </p>
 
         <div className="image">
-          <img src={confidentImage} />
+          <img src="http://1.bp.blogspot.com/-icFd4ptlDD8/VdzQO8VKmjI/AAAAAAAAuKQ/aBv9-nZz2i0/s1600/20150819_100239.jpg" />
           <p>
-            Lighthouse Montessori Academy children grind corn as a practical
-            life activity as they study Native Americans
+            A child uses 10 blocks in each set to compare how the minor
+            differences in height, with, or length result in notable differences
+            in the towers that can be built
+          </p>
+        </div>
+
+        <div className="image">
+          <img src="http://3.bp.blogspot.com/--IKkqp0zvcM/VdzQZHoOy0I/AAAAAAAAuKY/ngOP35Eg0dc/s1600/IMG_20150728_210119280.jpg" />
+          <p>
+            A child uses 10 cubes from 1cm to 10 cm, along with a 10 brown
+            prisms of the same length but differing in height, to build a
+            structure.
+          </p>
+        </div>
+
+        <div className="image">
+          <img src="http://4.bp.blogspot.com/-iYnNuHlGxcE/VdzUOdFemdI/AAAAAAAAuK4/yAD91g583jk/s1600/IMG_20150728_211008970_TOP.jpg" />
+          <p>
+            The child uses the same pink cubes and brown prisms to explore a
+            different structure.
+          </p>
+        </div>
+
+        <div className="image">
+          <img src="http://3.bp.blogspot.com/-way1OljS12w/VdzQnFQXISI/AAAAAAAAuKg/PAsRRT2_peg/s1600/20150722_084528.jpg" />
+          <p>
+            A child builds a tower, realizing that for stability, the biggest
+            blocks must come first.
+          </p>
+        </div>
+
+        <div className="image">
+          <img src="http://3.bp.blogspot.com/-NU4ZEb1N9Lg/VdzQ6DIchII/AAAAAAAAuKo/cNHlZ31RLVA/s1600/20150715_084258.jpg" />
+          <p>
+            A child explores how the differences in density between a marble and
+            a pompom result in different speeds.
           </p>
         </div>
 
